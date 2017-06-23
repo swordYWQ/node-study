69c2c938e2aea539dc4f4de99ecfe5b643efcfe3
swordYWQ <86549505@qq.com>
2017-06-23 14:35:48 +0800

    readme

--- src/api/user.js
+++ src/api/user.js
@@ -4,32 +4,38 @@ var resDataUtil = require('../util/resDataUtil')
 
 const loginIn = (req, res) => {
     var param = req.query;
-    var sql = "select * from simplebase.user where userName='" + param.userName + "'"
-    dbConnection.mysqlDB.execQuery(sql, (result) => {
-        if (result.length > 0) {
-            for (var i = 0; i < result.length; i++) {
-                if (param.userName == result[i].userName && param.password == result[i].password) {
+    if (param.userName && param.password) {
+        var sql = "select * from simplebase.user where userName='" + param.userName + "'"
+        dbConnection.mysqlDB.execQuery(sql, (result) => {
+            if (result.length > 0) {
+                for (var i = 0; i < result.length; i++) {
+                    if (param.userName == result[i].userName && param.password == result[i].password) {
 
-                    var user = {
-                        username: param.userName,
-                        password: param.password
-                    };
-                    req.session.user = user;
-                    log.info("用户:" + param.userName + " 密码:" + param.password + "登录IP:" + req._remoteAddress + " 登陆成功!")
-                    res.end(resDataUtil.success({
-                        userName: param.userName
-                    }));
-                } else {
-                    log.warn("用户:" + param.userName + " 密码:" + param.password + "登录IP:" + req._remoteAddress + "用户名或密码错误!")
-                    res.end(resDataUtil.error([], ' 用户名或密码错误!'));
+                        var user = {
+                            username: param.userName,
+                            password: param.password
+                        };
+                        req.session.user = user;
+                        log.info("用户:" + param.userName + " 密码:" + param.password + "登录IP:" + req._remoteAddress + " 登陆成功!")
+                        res.end(resDataUtil.success({
+                            userName: param.userName
+                        }));
+                    } else {
+                        log.warn("用户:" + param.userName + " 密码:" + param.password + "登录IP:" + req._remoteAddress + "用户名或密码错误!")
+                        res.end(resDataUtil.error([], ' 用户名或密码错误!'));
+                    }
                 }
-            }
 
-        } else {
-            log.warn("用户:" + param.userName + " 密码:" + param.password + "登录IP:" + req._remoteAddress + " 用户名不存在!")
-            res.end(resDataUtil.error([], '用户名不存在!'));
-        }
-    })
+            } else {
+                log.warn("用户:" + param.userName + " 密码:" + param.password + "登录IP:" + req._remoteAddress + " 用户名不存在!")
+                res.end(resDataUtil.error([], '用户名不存在!'));
+            }
+        })
+    }
+    else{
+        log.warn("用户:" + param.userName + " 密码:" + param.password + "登录IP:" + req._remoteAddress + " 参数错误!")
+                res.end(resDataUtil.error([], '参数错误!'));
+    }
 }
 
 const loginOut = (req, res) => {
